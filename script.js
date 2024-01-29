c1a6e52cccabe438d67929dd5753340fdafc48fe
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2024-01-29 13:57:31 +0100

    alapok

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let player = document.querySelector(".player");
+  let level; // kulonbozo szimbolumok jelzik hogy mi hol van
+
+  //document.addEventListener('keydown', mozgas);
+});
