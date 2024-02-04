247752218d73360e367578d84024fce8240021ee
feher-kristof1 <feher.kristof1@students.jedlik.eu>
2024-02-04 15:20:33 +0100

    1. szint done

--- blockok.js
+++ blockok.js
@@ -1,100 +1,100 @@
-const blockokcl = [
-    {
-        "x" : 130,
-        "y" : 123,
-        "width" : 50,
-        "height" : 3,
-    },
-    {
-        "x" : 35,
-        "y" : 113,
-        "width" : 35,
-        "height" : 3,
-    },
-    {
-        "x" : 95,
-        "y" : 103,
-        "width" : 35,
-        "height" : 3,
-    },
-    {
-        "x" : 5,
-        "y" : 93,
-        "width" : 35,
-        "height" : 3,
-    },
-    {
-        "x" : 75,
-        "y" : 70,
-        "width" : 75,
-        "height" : 3,
-    },
-    {
-        "x" : 55,
-        "y" : 75,
-        "width" : 3,
-        "height" : 3,
-    },
-    {
-        "x" : 250,
-        "y" : 10,
-        "width" : 50,
-        "height" : 3,
-    },
-    {
-        "x" : 175,
-        "y" : 60,
-        "width" : 3,
-        "height" : 3,
-    },
-    {
-        "x" : 185,
-        "y" : 50,
-        "width" : 3,
-        "height" : 3,
-    },
-    {
-        "x" : 195,
-        "y" : 40,
-        "width" : 3,
-        "height" : 3,
-    },
-    {
-        "x" : 205,
-        "y" : 30,
-        "width" : 3,
-        "height" : 3,
-    },
-    {
-        "x" : 140,
-        "y" : 15,
-        "width" : 50,
-        "height" : 3,
-    },
-    {
-        "x" : 60,
-        "y" : 30,
-        "width" : 50,
-        "height" : 3,
-    },
-    {
-        "x" : 0,
-        "y" : 20,
-        "width" : 50,
-        "height" : 3,
-    },
-    {
-        "x" : 60,
-        "y" : 5,
-        "width" : 180,
-        "height" : 3,
-    },
-    {
-        "x" : 260,
-        "y" : 103,
-        "width" : 10,
-        "height" : 3,
-    },
-]
+const lvl1 = [
+  {
+    x: 130,
+    y: 123,
+    width: 50,
+    height: 3,
+  },
+  {
+    x: 35,
+    y: 113,
+    width: 35,
+    height: 3,
+  },
+  {
+    x: 95,
+    y: 103,
+    width: 35,
+    height: 3,
+  },
+  {
+    x: 5,
+    y: 93,
+    width: 35,
+    height: 3,
+  },
+  {
+    x: 75,
+    y: 70,
+    width: 75,
+    height: 3,
+  },
+  {
+    x: 47,
+    y: 75,
+    width: 9,
+    height: 3,
+  },
+  {
+    x: 250,
+    y: 10,
+    width: 50,
+    height: 3,
+  },
+  {
+    x: 165,
+    y: 60,
+    width: 9,
+    height: 3,
+  },
+  {
+    x: 180,
+    y: 50,
+    width: 9,
+    height: 3,
+  },
+  {
+    x: 190,
+    y: 40,
+    width: 9,
+    height: 3,
+  },
+  {
+    x: 200,
+    y: 30,
+    width: 9,
+    height: 3,
+  },
+  {
+    x: 140,
+    y: 15,
+    width: 50,
+    height: 3,
+  },
+  {
+    x: 60,
+    y: 30,
+    width: 70,
+    height: 3,
+  },
+  {
+    x: 0,
+    y: 20,
+    width: 50,
+    height: 3,
+  },
+  {
+    x: 45,
+    y: 5,
+    width: 200,
+    height: 3,
+  },
+  {
+    x: 260,
+    y: 103,
+    width: 10,
+    height: 3,
+  },
+];
 //Fel kell majd tölteni a platformok adataival, az x,y a bal felső sarkukat jelöli, y lefelé nő, a téglalap teteje a 0
-export {blockokcl}
+export { lvl1 };
---
+++ lvl1.js
@@ -0,0 +1,136 @@
+import { lvl1 } from "./blockok.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let player = document.querySelector(".player");
+  let level; // kulonbozo szimbolumok jelzik hogy mi hol van
+
+  let jatekter = document.querySelector("#menu");
+  let blockok = document.querySelector("#block");
+
+  let char = jatekter.getContext("2d");
+  let kozga = blockok.getContext("2d");
+
+  let x = 10;
+  let y = 139;
+  let width = 4;
+  let height = 3;
+
+  let velxj = 0;
+  let velxb = 0;
+  let vely = 0;
+
+  let speed = 1;
+  let jumpstrenght = 25;
+
+  let grav = 0.5;
+  let coll = false;
+
+  addEventListener("keydown", function (e) {
+    if (e.code == "KeyD") velxj = +speed;
+    if (e.code == "KeyA") velxb = -speed;
+    if (e.code == "ArrowRight") velxj = speed;
+    if (e.code == "ArrowLeft") velxb = -speed;
+    if (e.code == "Space") {
+      if (e.code == "Space") {
+        if (coll) {
+          for (let i = 0; i < jumpstrenght; i += 0.01) {
+            for (let j = 0; j < lvl1.length; j++) {
+              let block = lvl1[j];
+              let bx = block.x;
+              let by = block.y;
+              let bwidth = block.width;
+              let bheight = block.height;
+
+              if (
+                y <= by + bheight &&
+                y + height >= by + bheight &&
+                x + width >= bx &&
+                x <= bx + bwidth
+              ) {
+                coll = true;
+                y = by + bheight - 1;
+              }
+            }
+            y -= 0.01;
+          }
+        }
+      }
+    }
+  });
+
+  addEventListener("keyup", function (e) {
+    if (e.code == "KeyD") velxj = 0;
+    if (e.code == "KeyA") velxb = 0;
+    if (e.code == "ArrowRight") velxj = 0;
+    if (e.code == "ArrowLeft") velxb = 0;
+  });
+
+  function update() {
+    if (coll == false) grav = 0.65;
+    x += velxb;
+    x += velxj;
+    y += vely + grav;
+    coll = false;
+
+    for (let i = 0; i < lvl1.length; i++) {
+      let block = lvl1[i];
+      let bx = block.x;
+      let by = block.y;
+      let bwidth = block.width;
+      let bheight = block.height;
+
+      kozga.fillRect(bx, by, bwidth, bheight);
+      kozga.fillStyle = "red";
+
+      if (
+        (y + height >= by &&
+          y + height <= by + bheight &&
+          x + width >= bx &&
+          x + width <= bx + bwidth + 3) ||
+        (y + height <= by &&
+          y + height <= by + bheight &&
+          x >= bx &&
+          x <= bx + bwidth + 5)
+      ) {
+        if (y < by && y > by - bheight && y + height > by - bheight) {
+          y = by - height;
+          coll = true;
+        }
+        if (y < by - bheight) {
+          coll = false;
+        }
+        if (y > by && y + height > by) {
+          y = by + bheight;
+          coll = true;
+        }
+        if (y > by + bheight && x > bx && x < bx + bwidth) {
+          y = by + bheight - 1;
+          coll = true;
+        }
+      }
+      if (x + width > 285 && y < 10) {
+        document.querySelector("#cel").innerHTML = "Gratula, nyertél!";
+        velxb = 0;
+        velxj = 0;
+      }
+      if (x + width < 285 && y < 10) {
+        document.querySelector("#cel").innerHTML = "";
+      }
+    }
+
+    char.clearRect(0, 0, jatekter.width, jatekter.height);
+    char.fillRect(x, y, width, height);
+
+    if (y + 2 > 140) {
+      y = 139;
+      grav = 0;
+      coll = true;
+    }
+    if (y < 0) y = 0;
+
+    requestAnimationFrame(update);
+    console.log(x, y);
+  }
+
+  update();
+});
